888602241be6e78d5cd56861dff9dee1982675bd
hanbing <bing.han@transwarp.io>
2018-08-02 18:34:45 +0800

    update swag vendor

---
+++ vendor/github.com/hohice/swag/doc.go
@@ -0,0 +1,5 @@
+/*
+Package swag converts Go annotations to Swagger Documentation 2.0.
+See https://github.com/hohice/swag for more information about swag.
+*/
+package swag // import "github.com/hohice/swag"
