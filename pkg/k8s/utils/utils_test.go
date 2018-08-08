d6de8dfbaf3684ac77768c90409d545b349be138
yang.cong <yang.cong@transwarp.io>
2018-08-08 19:48:48 +0800

    get node by label selector

--- pkg/k8s/utils/utils_test.go
+++ pkg/k8s/utils/utils_test.go
@@ -1,6 +1,9 @@
 package utils
 
-import "testing"
+import (
+	"testing"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 func TestMergeLabels(t *testing.T) {
 	labels := MergeLabels(nil, map[string]string{"test1": "test1", "test2": "test2"}, nil)
@@ -35,3 +38,13 @@ func TestMergeLabels(t *testing.T) {
 		t.Fail()
 	}
  }
+
+func TestConvertLabelSelectorToStr(t *testing.T) {
+	 str, err := ConvertLabelSelectorToStr(&v1.LabelSelector{MatchLabels: map[string]string{"test1": "test1", "test2": "test2"}})
+	 if err != nil {
+	 	t.Fail()
+	 }
+	if str != "test1=test1,test2=test2" {
+		t.Fail()
+	}
+}
