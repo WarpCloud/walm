edd82cdf72ed8bc699a0ad3941786d0994efec6b
yang.cong <yang.cong@transwarp.io>
2019-03-05 18:25:34 +0800

    #WARP-31750 support to install release with meta info pretty parameters

--- pkg/release/metainfo_types.go
+++ pkg/release/metainfo_types.go
@@ -1,6 +1,11 @@
 package release
 
-import "k8s.io/helm/pkg/walm"
+import (
+	"k8s.io/helm/pkg/walm"
+	"github.com/tidwall/sjson"
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+)
 
 // chart metainfo
 type ChartMetaInfo struct {
@@ -10,6 +15,101 @@ type ChartMetaInfo struct {
 	ChartRoles            []*MetaRoleConfig          `json:"roles"`
 	ChartParams           []*MetaCommonConfig        `json:"params"`
 	Plugins               []*walm.WalmPlugin         `json:"plugins"`
+}
+
+type MetaInfoParams struct {
+	Params []*MetaCommonConfigValue `json:"params"`
+	Roles  []*MetaRoleConfigValue   `json:"roles"`
+}
+
+func commonConfigMapping(params []*MetaCommonConfigValue, chartParams []*MetaCommonConfig) map[string]interface{} {
+	mapping := map[string]interface{}{}
+
+	chartParamsMap := map[string]*MetaCommonConfig{}
+	for _, chartParam := range chartParams {
+		chartParamsMap[chartParam.Name] = chartParam
+	}
+
+	for _, param := range params {
+		if chartParam, ok := chartParamsMap[param.Name]; ok {
+			mapping[chartParam.MapKey] = param.Value
+		}
+	}
+
+	return mapping
+}
+
+func roleConfigMapping(roles []*MetaRoleConfigValue, chartRoles []*MetaRoleConfig) map[string]interface{} {
+	mapping := map[string]interface{}{}
+	chartRoleMap := map[string]*MetaRoleConfig{}
+	for _, chartRole := range chartRoles {
+		chartRoleMap[chartRole.Name] = chartRole
+	}
+
+	for _, role := range roles {
+		if chartRole, ok := chartRoleMap[role.Name]; ok {
+			chartRoleBaseConfigMap := map[string]*MetaCommonConfig{}
+			for _, chartRoleBaseConfig := range chartRole.RoleBaseConfig {
+				chartRoleBaseConfigMap[chartRoleBaseConfig.Name] = chartRoleBaseConfig
+			}
+
+			for _, roleBaseConfig := range role.RoleBaseConfig {
+				if chartRoleBaseConfig, ok := chartRoleBaseConfigMap[roleBaseConfig.Name]; ok {
+					mapping[chartRoleBaseConfig.MapKey] = roleBaseConfig.Value
+				}
+			}
+
+			mapping[chartRole.RoleResourceConfig.LimitsCpuKey] = role.RoleResourceConfig.LimitsCpuKey
+			mapping[chartRole.RoleResourceConfig.LimitsGpuKey] = role.RoleResourceConfig.LimitsGpuKey
+			mapping[chartRole.RoleResourceConfig.RequestsCpuKey] = role.RoleResourceConfig.RequestsCpuKey
+			mapping[chartRole.RoleResourceConfig.RequestsGpuKey] = role.RoleResourceConfig.RequestsGpuKey
+			mapping[chartRole.RoleResourceConfig.RequestsMemoryKey] = role.RoleResourceConfig.RequestsMemoryKey
+			mapping[chartRole.RoleResourceConfig.LimitsMemoryKey] = role.RoleResourceConfig.LimitsMemoryKey
+
+			chartStorageConfigMap := map[string]*MetaCommonConfig{}
+			for _, chartStorageConfig := range chartRole.RoleResourceConfig.StorageResources {
+				chartStorageConfigMap[chartStorageConfig.Name] = chartStorageConfig
+			}
+
+			for _, storageConfig := range role.RoleResourceConfig.StorageResources {
+				if chartStorageConfig, ok := chartStorageConfigMap[storageConfig.Name]; ok {
+					mapping[chartStorageConfig.MapKey] = storageConfig.Value
+				}
+			}
+		}
+	}
+
+	return mapping
+}
+
+func (metaInfoParams *MetaInfoParams) ToConfigValues(metaInfo *ChartMetaInfo) (configValues map[string]interface{}, err error) {
+	jsonStr := "{}"
+	mapping := commonConfigMapping(metaInfoParams.Params, metaInfo.ChartParams)
+	for key, value := range mapping {
+		jsonStr, err = sjson.Set(jsonStr, key, value)
+		if err != nil {
+			logrus.Errorf("failed to set json : %s", err.Error())
+			return
+		}
+	}
+
+	mapping = roleConfigMapping(metaInfoParams.Roles, metaInfo.ChartRoles)
+	for key, value := range mapping {
+		jsonStr, err = sjson.Set(jsonStr, key, value)
+		if err != nil {
+			logrus.Errorf("failed to set json : %s", err.Error())
+			return
+		}
+	}
+
+	configValues = map[string]interface{}{}
+	err = json.Unmarshal([]byte(jsonStr), &configValues)
+	if err != nil {
+		logrus.Errorf("failed to unmarshal config values : %s", err.Error())
+		return nil, err
+	}
+
+	return
 }
 
 type ChartDependencyMetaInfo struct {
@@ -32,13 +132,29 @@ type MetaCommonConfig struct {
 	Required     bool        `json:"required" description:"required"`
 }
 
+type MetaCommonConfigValue struct {
+	Name  string      `json:"name" description:"config name"`
+	Value interface{} `json:"value" description:"config value"`
+}
+
 type MetaResourceConfig struct {
-	LimitsMemoryKey   string `json:"limitsMemoryKey" description:"resource memory limit"`
-	LimitsCpuKey      string `json:"limitsCpuKey" description:"resource cpu limit"`
-	LimitsGpuKey      string `json:"limitsGpuKey" description:"resource gpu limit"`
-	RequestsMemoryKey string `json:"requestsMemoryKey" description:"resource memory request"`
-	RequestsCpuKey    string `json:"limitsCpuKey" description:"resource cpu request"`
-	RequestsGpuKey    string `json:"limitsGpuKey" description:"resource gpu request"`
+	LimitsMemoryKey   string              `json:"limitsMemoryKey" description:"resource memory limit"`
+	LimitsCpuKey      string              `json:"limitsCpuKey" description:"resource cpu limit"`
+	LimitsGpuKey      string              `json:"limitsGpuKey" description:"resource gpu limit"`
+	RequestsMemoryKey string              `json:"requestsMemoryKey" description:"resource memory request"`
+	RequestsCpuKey    string              `json:"requestsCpuKey" description:"resource cpu request"`
+	RequestsGpuKey    string              `json:"requestsGpuKey" description:"resource gpu request"`
+	StorageResources  []*MetaCommonConfig `json:"storageResources" description:"resource storage request"`
+}
+
+type MetaResourceConfigValue struct {
+	LimitsMemoryKey   interface{}              `json:"limitsMemoryKey" description:"resource memory limit"`
+	LimitsCpuKey      interface{}              `json:"limitsCpuKey" description:"resource cpu limit"`
+	LimitsGpuKey      interface{}              `json:"limitsGpuKey" description:"resource gpu limit"`
+	RequestsMemoryKey interface{}              `json:"requestsMemoryKey" description:"resource memory request"`
+	RequestsCpuKey    interface{}              `json:"requestsCpuKey" description:"resource cpu request"`
+	RequestsGpuKey    interface{}              `json:"requestsGpuKey" description:"resource gpu request"`
+	StorageResources  []*MetaCommonConfigValue `json:"storageResources" description:"resource storage request"`
 }
 
 type MetaHealthProbeConfig struct {
@@ -57,4 +173,11 @@ type MetaRoleConfig struct {
 	RoleBaseConfig        []*MetaCommonConfig    `json:"baseConfig"`
 	RoleResourceConfig    *MetaResourceConfig    `json:"resources"`
 	RoleHealthCheckConfig *MetaHealthCheckConfig `json:"healthChecks"`
+}
+
+type MetaRoleConfigValue struct {
+	Name               string                   `json:"name"`
+	RoleBaseConfig     []*MetaCommonConfigValue `json:"baseConfig"`
+	RoleResourceConfig *MetaResourceConfigValue `json:"resources"`
+	// TODO healthChecks
 }
