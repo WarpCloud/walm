0e2a63c359f77a3e7e4add1ba47a1f065e61ad00
yang.cong <yang.cong@transwarp.io>
2019-03-22 17:10:43 +0800

    add client host in router log

--- router/middleware/routerlogs.go
+++ router/middleware/routerlogs.go
@@ -9,5 +9,5 @@ import (
 func RouteLogging(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
 	now := time.Now()
 	chain.ProcessFilter(req, resp)
-	logrus.Infof("[route-filter (logger)] OP %s URI %s COST %v RESP %d", req.Request.Method, req.Request.URL, time.Now().Sub(now), resp.StatusCode())
+	logrus.Infof("[route-filter (logger)] CLIENT %s OP %s URI %s COST %v RESP %d", req.Request.Host, req.Request.Method, req.Request.URL, time.Now().Sub(now), resp.StatusCode())
 }
