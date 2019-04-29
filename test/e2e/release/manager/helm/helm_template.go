09991dbe84cdb74f77d506be8e72a25dc480b2cd
yang.cong <yang.cong@transwarp.io>
2019-04-29 16:56:07 +0800

    #WARP-33220 add e2e test

--- test/e2e/release/manager/helm/helm_template.go
+++ test/e2e/release/manager/helm/helm_template.go
@@ -8,6 +8,10 @@ import (
 	"walm/pkg/release"
 	"path/filepath"
 	"fmt"
+	"k8s.io/helm/pkg/chart/loader"
+	"k8s.io/helm/pkg/walm"
+	"k8s.io/helm/pkg/walm/plugins"
+	"encoding/json"
 )
 
 var _ = Describe("ReleaseDryRun", func() {
@@ -16,6 +20,8 @@ var _ = Describe("ReleaseDryRun", func() {
 		namespace  string
 		helmClient *helm.HelmClient
 		chartPath  string
+		chartFiles []*loader.BufferedFile
+		releaseRequest *release.ReleaseRequestV2
 		err        error
 	)
 
@@ -28,6 +34,13 @@ var _ = Describe("ReleaseDryRun", func() {
 		currentFilePath, err := framework.GetCurrentFilePath()
 		Expect(err).NotTo(HaveOccurred())
 		chartPath = filepath.Join(filepath.Dir(currentFilePath), "../../../../resources/release/manager/helm/tomcat-0.2.0.tgz")
+		chartFiles, err = framework.LoadChartArchive(chartPath)
+		Expect(err).NotTo(HaveOccurred())
+		releaseRequest = &release.ReleaseRequestV2{
+			ReleaseRequest: release.ReleaseRequest{
+				Name: "tomcat-test",
+			},
+		}
 	})
 
 	AfterEach(func() {
@@ -39,17 +52,30 @@ var _ = Describe("ReleaseDryRun", func() {
 	It("test release dry run", func() {
 
 		By("dry run creating release")
-		chartFiles, err := framework.LoadChartArchive(chartPath)
-		Expect(err).NotTo(HaveOccurred())
-
-		releaseRequest := &release.ReleaseRequestV2{
-			ReleaseRequest: release.ReleaseRequest{
-				Name: "tomcat-test",
-			},
-		}
 		manifestStr, err := helmClient.DryRunRelease(namespace, releaseRequest, false, chartFiles)
 		Expect(err).NotTo(HaveOccurred())
-		expectedManifestStr := fmt.Sprintf("\n---\n# Source: tomcat/templates/appsrv-svc.yaml\napiVersion: v1\nkind: Service\nmetadata:\n  name: tomcat-test\n  labels:\n    app: tomcat\n    chart: tomcat-0.2.0\n    release: tomcat-test\n    heritage: Helm\nspec:\n  type: NodePort\n  ports:\n    - port: 80\n      targetPort: 8080\n      protocol: TCP\n      name: http\n  selector:\n    app: tomcat\n    release: tomcat-test\n---\n# Source: tomcat/templates/appsrv.yaml\napiVersion: apps/v1beta2\nkind: Deployment\nmetadata:\n  name: tomcat-test\n  labels:\n    app: tomcat\n    chart: tomcat-0.2.0\n    release: tomcat-test\n    heritage: Helm\nspec:\n  replicas: 1\n  selector:\n    matchLabels:\n      app: tomcat\n      release: tomcat-test\n  template:\n    metadata:\n      labels:\n        app: tomcat\n        release: tomcat-test\n    spec:\n      volumes:\n        - name: app-volume\n          emptyDir: {}\n      initContainers:\n        - name: war\n          image: ananwaresystems/webarchive:1.0\n          imagePullPolicy: Always\n          command:\n            - \"sh\"\n            - \"-c\"\n            - \"cp /*.war /app\"\n          volumeMounts:\n            - name: app-volume\n              mountPath: /app\n      containers:\n        - name: tomcat\n          image: tomcat:7.0\n          imagePullPolicy: Always\n          volumeMounts:\n            - name: app-volume\n              mountPath: /usr/local/tomcat/webapps\n          ports:\n            - containerPort: 8080\n              hostPort: 8009\n          livenessProbe:\n            httpGet:\n              path: /sample\n              port: 8080\n            initialDelaySeconds: 60\n            periodSeconds: 30\n          readinessProbe:\n            httpGet:\n              path: /sample\n              port: 8080\n            initialDelaySeconds: 60\n            periodSeconds: 30\n            failureThreshold: 6\n          resources:\n            limits:\n              cpu: 4\n              memory: 4Gi\n            requests:\n              cpu: 2\n              memory: 2Gi\n---\n# Source: tomcat/autogen-releaseconfig.json.transwarp-jsonnet.yaml\napiVersion: apiextensions.transwarp.io/v1beta1\nkind: ReleaseConfig\nmetadata:\n  creationTimestamp: null\n  labels:\n    auto-gen: \"true\"\n  name: tomcat-test\n  namespace: %s\nspec:\n  chartAppVersion: \"7\"\n  chartName: tomcat\n  chartVersion: 0.2.0\n  configValues: {}\n  dependencies: {}\n  dependenciesConfigValues: {}\n  outputConfig: {}\n  repo: \"\"\nstatus: {}", namespace)
+		expectedManifestStr := fmt.Sprintf("\n---\napiVersion: v1\nkind: Service\nmetadata:\n  labels:\n    app: tomcat\n    chart: tomcat-0.2.0\n    heritage: Helm\n    release: tomcat-test\n  name: tomcat-test\n  namespace: %s\nspec:\n  ports:\n  - name: http\n    port: 80\n    protocol: TCP\n    targetPort: 8080\n  selector:\n    app: tomcat\n    release: tomcat-test\n  type: NodePort\n\n---\napiVersion: apps/v1beta2\nkind: Deployment\nmetadata:\n  labels:\n    app: tomcat\n    chart: tomcat-0.2.0\n    heritage: Helm\n    release: tomcat-test\n  name: tomcat-test\n  namespace: %s\nspec:\n  replicas: 1\n  selector:\n    matchLabels:\n      app: tomcat\n      release: tomcat-test\n  template:\n    metadata:\n      labels:\n        app: tomcat\n        release: tomcat-test\n    spec:\n      containers:\n      - image: tomcat:7.0\n        imagePullPolicy: Always\n        livenessProbe:\n          httpGet:\n            path: /sample\n            port: 8080\n          initialDelaySeconds: 60\n          periodSeconds: 30\n        name: tomcat\n        ports:\n        - containerPort: 8080\n          hostPort: 8009\n        readinessProbe:\n          failureThreshold: 6\n          httpGet:\n            path: /sample\n            port: 8080\n          initialDelaySeconds: 60\n          periodSeconds: 30\n        resources:\n          limits:\n            cpu: 4\n            memory: 4Gi\n          requests:\n            cpu: 2\n            memory: 2Gi\n        volumeMounts:\n        - mountPath: /usr/local/tomcat/webapps\n          name: app-volume\n      initContainers:\n      - command:\n        - sh\n        - -c\n        - cp /*.war /app\n        image: ananwaresystems/webarchive:1.0\n        imagePullPolicy: Always\n        name: war\n        volumeMounts:\n        - mountPath: /app\n          name: app-volume\n      volumes:\n      - emptyDir: {}\n        name: app-volume\n\n---\napiVersion: apiextensions.transwarp.io/v1beta1\nkind: ReleaseConfig\nmetadata:\n  creationTimestamp: null\n  labels:\n    auto-gen: \"true\"\n  name: tomcat-test\n  namespace: %s\nspec:\n  chartAppVersion: \"7\"\n  chartName: tomcat\n  chartVersion: 0.2.0\n  configValues: {}\n  dependencies: {}\n  dependenciesConfigValues: {}\n  outputConfig: {}\n  repo: \"\"\nstatus: {}\n", namespace, namespace, namespace)
+		Expect(manifestStr).To(Equal(expectedManifestStr))
+
+	})
+
+	It("test release dry run: plugins", func() {
+
+		By("dry run creating release with plugins")
+		lablePodArgs := &plugins.LabelPodArgs{
+			LabelsToAdd: map[string]string{
+				"test_key": "test_value",
+			},
+		}
+		labelPodArgsBytes, err := json.Marshal(lablePodArgs)
+		Expect(err).NotTo(HaveOccurred())
+		releaseRequest.Plugins = append(releaseRequest.Plugins, &walm.WalmPlugin{
+			Name: plugins.LabelPodPluginName,
+			Args: string(labelPodArgsBytes),
+		})
+		manifestStr, err := helmClient.DryRunRelease(namespace, releaseRequest, false, chartFiles)
+		Expect(err).NotTo(HaveOccurred())
+		expectedManifestStr := fmt.Sprintf("\n---\napiVersion: v1\nkind: Service\nmetadata:\n  labels:\n    app: tomcat\n    chart: tomcat-0.2.0\n    heritage: Helm\n    release: tomcat-test\n  name: tomcat-test\n  namespace: %s\nspec:\n  ports:\n  - name: http\n    port: 80\n    protocol: TCP\n    targetPort: 8080\n  selector:\n    app: tomcat\n    release: tomcat-test\n  type: NodePort\n\n---\napiVersion: extensions/v1beta1\nkind: Deployment\nmetadata:\n  creationTimestamp: null\n  labels:\n    app: tomcat\n    chart: tomcat-0.2.0\n    heritage: Helm\n    release: tomcat-test\n  name: tomcat-test\n  namespace: %s\nspec:\n  replicas: 1\n  selector:\n    matchLabels:\n      app: tomcat\n      release: tomcat-test\n  strategy: {}\n  template:\n    metadata:\n      creationTimestamp: null\n      labels:\n        app: tomcat\n        release: tomcat-test\n        test_key: test_value\n    spec:\n      containers:\n      - image: tomcat:7.0\n        imagePullPolicy: Always\n        livenessProbe:\n          httpGet:\n            path: /sample\n            port: 8080\n          initialDelaySeconds: 60\n          periodSeconds: 30\n        name: tomcat\n        ports:\n        - containerPort: 8080\n          hostPort: 8009\n        readinessProbe:\n          failureThreshold: 6\n          httpGet:\n            path: /sample\n            port: 8080\n          initialDelaySeconds: 60\n          periodSeconds: 30\n        resources:\n          limits:\n            cpu: \"4\"\n            memory: 4Gi\n          requests:\n            cpu: \"2\"\n            memory: 2Gi\n        volumeMounts:\n        - mountPath: /usr/local/tomcat/webapps\n          name: app-volume\n      initContainers:\n      - command:\n        - sh\n        - -c\n        - cp /*.war /app\n        image: ananwaresystems/webarchive:1.0\n        imagePullPolicy: Always\n        name: war\n        resources: {}\n        volumeMounts:\n        - mountPath: /app\n          name: app-volume\n      volumes:\n      - emptyDir: {}\n        name: app-volume\nstatus: {}\n\n---\napiVersion: apiextensions.transwarp.io/v1beta1\nkind: ReleaseConfig\nmetadata:\n  creationTimestamp: null\n  labels:\n    auto-gen: \"true\"\n  name: tomcat-test\n  namespace: %s\nspec:\n  chartAppVersion: \"7\"\n  chartName: tomcat\n  chartVersion: 0.2.0\n  configValues: {}\n  dependencies: {}\n  dependenciesConfigValues: {}\n  outputConfig: {}\n  repo: \"\"\nstatus: {}\n", namespace, namespace, namespace)
 		Expect(manifestStr).To(Equal(expectedManifestStr))
 
 	})
