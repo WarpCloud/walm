fb375f5fae5f1aa817ca1c04a6905204d4c274c6
yang.cong <yang.cong@transwarp.io>
2019-07-18 16:33:49 +0800

    update to_github.sh

--- github/to_github.sh
+++ github/to_github.sh
@@ -5,12 +5,11 @@ export SOURCE_BRANCH=dev
 export TARGET_BRANCH=master
 git clone https://github.com/WarpCloud/walm.git
 cd walm
+git checkout $TARGET_BRANCH
 git remote add $TRUNK ssh://git@172.16.1.41:10022/TDC/WALM.git
 git fetch --all
 git merge -s ours --no-commit $TRUNK/$SOURCE_BRANCH
 for i in `cat .mirror`; do git checkout $TRUNK/$SOURCE_BRANCH -- $i; done
-git config --global user.name "transwarp"
-git config --global user.email "transwarp-community@transwarp.io"
 CHANGED=`git status -s | wc -l`
 if [[ $CHANGED -gt 0 ]]; then git add . && git commit -m "Sync to mirror" && git push origin $TARGET_BRANCH; fi
 echo "SUCCESS"
